feaa036e4d80e1a07b4f6e82a897db15acfb25f3
GuilhermeAviacao <guilherme.aviacao@gmail.com>
2025-05-11 09:52:12 +0200

    Connect the Secret key

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_FLtliWBu8zP6@ep-sweet-fire-a28u7qhk.eu-central-1.aws.neon.tech/crust_thigh_write_565269")
+os.environ.setdefault("SECRET_KEY", "django-insecure--p=xiz)bpcav%b6n)1chotxp0f*uulx*$rwy2qny9dwx21fwno")
